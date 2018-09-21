176aab02f10654e93e49c73e3b09f2d41a32d718
Martin Hofmann <martin.hofmann@mni.thm.de>
2018-09-21 12:58:58 +0200

    irq: fix mask and unmask function swaped name

---
+++ drivers/irqchip/irq-eco32.c
@@ -0,0 +1,91 @@
+/*
+ * ECO32 Linux
+ *
+ * Linux architectural port borrowing liberally from similar works of
+ * others.  All original copyrights apply as per the original source
+ * declaration.
+ *
+ * Modifications for ECO32:
+ * Copyright (c) 2018 Hellwig Geisse, Martin Hofmann
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * irq.c -- Interrupt initialization and handling entry
+ */
+
+#include <linux/init.h>
+#include <linux/irq.h>
+#include <linux/irqchip.h>
+#include <linux/of_address.h>
+#include <linux/of_irq.h>
+
+#include <asm/irq.h>
+
+
+/*
+ * We safe the current state of active irqs here so we can
+ * load the active irqs from here.
+ *
+ * This needs be done so we can update the psw on each context
+ * switch and return so all threads run with the correct irqs
+ * masked
+ */
+unsigned long volatile irqmask = 0;
+
+
+static void eco32_unmask_irq(struct irq_data* data)
+{
+	irqmask |= (1 << data->irq);
+	or_irq_mask(1 << data->irq);
+}
+
+
+static void eco32_mask_irq(struct irq_data* data)
+{
+	irqmask &= ~(1 << data->irq);
+	and_irq_mask(~(1 << data->irq));
+}
+
+
+static struct irq_chip eco32_intc = {
+	.name = "ECO32",
+	.irq_mask = eco32_mask_irq,
+	.irq_unmask = eco32_unmask_irq,
+};
+
+
+static __init int eco32_irq_map(struct irq_domain *h, unsigned int irq,
+						  irq_hw_number_t hw_irq_num)
+{
+	irq_set_chip_and_handler(irq, &eco32_intc, handle_level_irq);
+
+    return 0;
+}
+
+static const struct irq_domain_ops irq_ops = {
+       .map    = eco32_irq_map,
+       .xlate  = irq_domain_xlate_onecell,
+};
+
+
+static int __init eco32_intc_of_init(struct device_node *intc,
+								   struct device_node *parent)
+{
+	struct irq_domain *domain;
+
+	domain = irq_domain_add_linear(intc, NR_IRQS, &irq_ops, NULL);
+	BUG_ON(!domain);
+	irq_set_default_host(domain);
+	
+	/*
+	 * FIXME: we allocate that timer irq befor we should..
+	 * need to look into this
+	 */
+	irq_create_mapping(domain, 15);
+	
+	return 0;
+}
+IRQCHIP_DECLARE(eco32_intc, "thm,eco32-intc", eco32_intc_of_init);
