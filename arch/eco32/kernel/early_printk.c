c275c281e1def245e34b97920405276454e14fe4
Martin Hofmann <martin.hofmann@mni.thm.de>
2018-09-23 14:00:18 +0200

    early_printk: added comment

--- arch/eco32/kernel/early_printk.c
+++ arch/eco32/kernel/early_printk.c
@@ -19,19 +19,30 @@
 #include <linux/console.h>
 
 
+/*
+ * The ECO32 early printk uses the first uart line
+ * to push out the messages.
+ * 
+ * this is a rather simple polling prinkt. but it is enough
+ * to get the chars out during early boot
+ * 
+ * once we sat up the serial line driver we switch to that
+ * anyways
+ */
+
 static void eco32_early_putc(char c)
 {
-	volatile unsigned int* base;
+    volatile unsigned int* base;
 
-	base = (unsigned int*) 0xF0300000;
+    base = (unsigned int*) 0xF0300000;
 
-	if (c == '\n') {
-		eco32_early_putc('\r');
-	}
+    if (c == '\n') {
+        eco32_early_putc('\r');
+    }
 
-	while ((*(base + 2) & 1) == 0) ;
+    while ((*(base + 2) & 1) == 0) ;
 
-	*(base + 3) = c;
+    *(base + 3) = c;
 }
 
 
@@ -39,26 +50,26 @@ static void eco32_early_puts(struct console* con,
                              const char* s,
                              unsigned n)
 {
-	while (n-- && *s) {
-		eco32_early_putc(*s++);
-	}
+    while (n-- && *s) {
+        eco32_early_putc(*s++);
+    }
 }
 
 
 static struct console eco32_earlycon = {
-	.name  = "eco32_earlycon",
-	.write = eco32_early_puts,
-	.flags = CON_PRINTBUFFER | CON_BOOT,
-	.index = -1,
+    .name  = "eco32_earlycon",
+    .write = eco32_early_puts,
+    .flags = CON_PRINTBUFFER | CON_BOOT,
+    .index = -1,
 };
 
 
 void __init setup_early_printk(void)
 {
-	if (early_console != NULL) {
-		return;
-	}
+    if (early_console != NULL) {
+        return;
+    }
 
-	early_console = &eco32_earlycon;
-	register_console(early_console);
+    early_console = &eco32_earlycon;
+    register_console(early_console);
 }
